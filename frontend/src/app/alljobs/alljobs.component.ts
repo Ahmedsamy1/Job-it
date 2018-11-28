f184fe01455ded3ea61052306a847f7638831ecc
mohamedmagdynady <mohammedmagdy989@ymail.com>
2018-11-28 17:46:21 +0200

    fixed something

--- frontend/src/app/alljobs/alljobs.component.ts
+++ frontend/src/app/alljobs/alljobs.component.ts
@@ -20,8 +20,11 @@ export class AlljobsComponent implements OnInit {
 
   }
 
-search() {
-  this.criteria = document.getElementById('1').value;
+search(event) {
+  event.preventDefault();
+console.log(event.srcElement[0].value);
+  this.criteria = event.srcElement[0].value;
+  console.log(this.criteria);
 console.log(this.criteria);
 this.http.get('https://jobs.github.com/positions.json?search=' + this.criteria).subscribe((data: {}) => {
       console.log(data);
